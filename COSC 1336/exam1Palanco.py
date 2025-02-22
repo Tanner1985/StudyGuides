30406223bd16c485ade82750b21cbabbfb34b1b9
Tanner <tanner.hoekstra1985@gmail.com>
2025-02-22 08:56:41 -0600

    Adding questions to quiz

--- COSC 1336/exam1Palanco.py
+++ COSC 1336/exam1Palanco.py
@@ -1,8 +1,8 @@
 #Simple script to ask questions and print answers for study
 def main():
-    questionNum = random_number()
     question_list = questionList()
     answer_list = answerList()
+    questionNum = random_question(len(question_list))
     answer = input(f'{question_list[questionNum]}: ')
     print(f'You answered: {answer}')
     print(f'The correct answer is: {answer_list[questionNum]}')
@@ -12,16 +12,17 @@ def main():
     else:
         print('Thank you for playing!')
 
-def random_number():
+def random_question(length):
     import random
-    random_number = random.randint(0,9)
+    random_number = random.randint(0,length)
     return random_number
 
 def questionList():
-    question_list = ['What is the binary representation of 185?', 'What is a key word in programing?']
+    question_list = ['What is the binary representation of 185?', 'What is a key word in programing?', 'What translates an assembly language program to a machine language program?', \
+                     'What are the rules that must be follwed when writing a program called?']
     return question_list
 def answerList():
-    answer_list = ['10111001','A predefined term in a programming language that is used for a specific operation' ]
+    answer_list = ['10111001','A predefined term in a programming language that is used for a specific operation', 'An assembler', 'Syntax' ]
     return answer_list
 
 main()
